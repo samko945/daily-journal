2a25460023fa17870efc968f1be7b5412436382f
Samuel <samkoj@hotmail.co.uk>
2021-05-07 16:00:50 +0100

    feat: add compose page + set post route

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const database = [];
+
 app.get("/", function(req, res) {
     res.render("home");
+})
+
+app.get("/compose", function(req, res) {
+    res.render("compose");
+})
+
+app.post("/compose", function(req, res) {
+    console.log(req.body)
+    res.redirect("/");
 })
 
 app.listen(5000, function() {
